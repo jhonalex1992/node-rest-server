1db91c5c940840de17e7f215efdb284f027110a6
unknown <jhonalex.92.js@gmail.com>
2020-05-18 21:30:13 -0500

    Rest server usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,17 @@
 // Puerto
 //=======================================
 process.env.PORT = process.env.PORT || 3000;
+//=======================================
+// Entorno
+//=======================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+//=======================================
+// Base de datos
+//=======================================
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://strider:VqsyXwvRUL0OuPK2@cluster0-d8ugc.mongodb.net/cafe'
+}
+process.env.URLDB = urlDB;
