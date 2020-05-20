94b1d40d9bfe9c707311f7d4d62c79f4b239edd4
unknown <jhonalex.92.js@gmail.com>
2020-05-19 23:06:57 -0500

    Login con JWT

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,10 +3,12 @@ const bcrypt = require('bcryptjs');
 const _ = require('underscore');
 
 const Usuario = require('../models/usuario');
+//middleware
+const { verificaToken, verificaAdminRole } = require('../middlewares/autenticacion')
 
 const app = express()
 
-app.get('/usuario', function (req, res) {
+app.get('/usuario', verificaToken, function (req, res) {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -34,7 +36,7 @@ app.get('/usuario', function (req, res) {
         })
 
 })
-app.post('/usuario', function (req, res) {
+app.post('/usuario', [verificaToken, verificaAdminRole], function (req, res) {
     let body = req.body;
 
     let usuario = new Usuario({
@@ -57,7 +59,7 @@ app.post('/usuario', function (req, res) {
         });
     });
 });
-app.put('/usuario/:id', function (req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdminRole], function (req, res) {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
     Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, ususarioBD) => {
@@ -74,7 +76,7 @@ app.put('/usuario/:id', function (req, res) {
     })
 
 })
-app.delete('/usuario/:id', function (req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdminRole], function (req, res) {
 
     let id = req.params.id;
     let cambiarEstado = {
